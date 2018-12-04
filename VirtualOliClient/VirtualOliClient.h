4ed20a4b94c679a1da0b278580a318f68d025bc6
Paul Dickens <dickens.paul@gmail.com>
2018-12-04 20:26:37 +1100

    Initial Commit

---
+++ VirtualOliClient/VirtualOliClient.h
@@ -0,0 +1,19 @@
+//
+//  VirtualOliClient.h
+//  VirtualOliClient
+//
+//  Created by Paul Dickens on 4/12/18.
+//  Copyright © 2018 thebookins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VirtualOliClient.
+FOUNDATION_EXPORT double VirtualOliClientVersionNumber;
+
+//! Project version string for VirtualOliClient.
+FOUNDATION_EXPORT const unsigned char VirtualOliClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VirtualOliClient/PublicHeader.h>
+
+
---
+++ VirtualOliClientTests/VirtualOliClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  VirtualOliClientTests.swift
+//  VirtualOliClientTests
+//
+//  Created by Paul Dickens on 4/12/18.
+//  Copyright © 2018 thebookins. All rights reserved.
+//
+
+import XCTest
+@testable import VirtualOliClient
+
+class VirtualOliClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
