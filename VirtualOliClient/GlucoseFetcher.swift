dc8613bfc564aa6c79078a6c63a627dd9f1ad533
Paul Dickens <dickens.paul@gmail.com>
2019-08-15 20:58:40 +1000

    change url

--- VirtualOliClient/GlucoseFetcher.swift
+++ VirtualOliClient/GlucoseFetcher.swift
@@ -10,8 +10,8 @@ import UIKit
 import LoopKit
 
 struct GlucoseFetcher {
-//    static let glucoseURL = "https://virtual-oli.herokuapp.com/api/cgm"
-    static let glucoseURL = "http://192.168.1.36:5000/api/cgms/5d42cc46e7179a064fabed43"
+    static let glucoseURL = "https://virtual-oli.herokuapp.com/api/cgms/5d42cc46e7179a064fabed43"
+//    static let glucoseURL = "http://192.168.1.36:5000/api/cgms/5d42cc46e7179a064fabed43"
     static let statusURL = "https://virtual-oli.herokuapp.com/api/pump/status"
     static let pumpURL = "https://virtual-oli.herokuapp.com/api/pump"
 
