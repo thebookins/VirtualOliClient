82b6b5dad9d8a1a556391e615bd2dadfd8d68309
Paul Dickens <dickens.paul@gmail.com>
2019-08-22 13:02:38 +1000

    add delete button

--- VirtualOliClientUI/VirtualPumpSettingsViewController.swift
+++ VirtualOliClientUI/VirtualPumpSettingsViewController.swift
@@ -42,17 +42,17 @@ class VirtualPumpSettingsViewController: UITableViewController {
 //    
     override public func viewDidLoad() {
         super.viewDidLoad()
-//
-//        title = cgmManager.localizedTitle
-//        
-//        tableView.rowHeight = UITableView.automaticDimension
-//        tableView.estimatedRowHeight = 44
-//        
-//        tableView.sectionHeaderHeight = UITableView.automaticDimension
-//        tableView.estimatedSectionHeaderHeight = 55
-//        
-//        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.className)
-//        tableView.register(TextButtonTableViewCell.self, forCellReuseIdentifier: TextButtonTableViewCell.className)
+        
+        title = "Pump Settings"
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44
+        
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+        tableView.estimatedSectionHeaderHeight = 55
+        
+        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.className)
+        tableView.register(TextButtonTableViewCell.self, forCellReuseIdentifier: TextButtonTableViewCell.className)
         
         let button = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneTapped(_:)))
         self.navigationItem.setRightBarButton(button, animated: false)
@@ -73,18 +73,18 @@ class VirtualPumpSettingsViewController: UITableViewController {
 //
 //    // MARK: - Table view data source
 //    
-//    private enum Section: Int {
-//        //        case authentication
-//        //        case latestReading
-//        case delete
-//        
-//        static let count = 1 //3
-//    }
-//    
-//    override public func numberOfSections(in tableView: UITableView) -> Int {
-//        return allowsDeletion ? Section.count : Section.count - 1
-//    }
-//    
+    private enum Section: Int, CaseIterable {
+        //        case authentication
+        //        case latestReading
+        case delete = 0
+        
+        static let count = 1 //3
+    }
+
+    override public func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.allCases.count
+    }
+//
 //    //    private enum LatestReadingRow: Int {
 //    //        case glucose
 //    //        case date
@@ -93,68 +93,68 @@ class VirtualPumpSettingsViewController: UITableViewController {
 //    //        static let count = 3
 //    //    }
 //    
-//    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        switch Section(rawValue: section)! {
-//            //        case .authentication:
-//            //            return 1
-//            //        case .latestReading:
-//        //            return LatestReadingRow.count
-//        case .delete:
-//            return 1
-//        }
-//    }
-//    
-//    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        switch Section(rawValue: indexPath.section)! {
-//            //        case .authentication:
-//            //            let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.className, for: indexPath) as! SettingsTableViewCell
-//            //
-//            //            let service = cgmManager.shareService
-//            //
-//            //            cell.textLabel?.text = LocalizedString("Credentials", comment: "Title of cell to set credentials")
-//            //            cell.detailTextLabel?.text = service.username ?? SettingsTableViewCell.TapToSetString
-//            //            cell.accessoryType = .disclosureIndicator
-//            //
-//            //            return cell
-//            //        case .latestReading:
-//            //            let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.className, for: indexPath) as! SettingsTableViewCell
-//            //            let glucose = cgmManager.latestBackfill
-//            //
-//            //            switch LatestReadingRow(rawValue: indexPath.row)! {
-//            //            case .glucose:
-//            //                cell.textLabel?.text = LocalizedString("Glucose", comment: "Title describing glucose value")
-//            //
-//            //                if let quantity = glucose?.quantity, let formatted = glucoseFormatter.string(from: quantity, for: glucoseUnit) {
-//            //                    cell.detailTextLabel?.text = formatted
-//            //                } else {
-//            //                    cell.detailTextLabel?.text = SettingsTableViewCell.NoValueString
-//            //                }
-//            //            case .date:
-//            //                cell.textLabel?.text = LocalizedString("Date", comment: "Title describing glucose date")
-//            //
-//            //                if let date = glucose?.timestamp {
-//            //                    cell.detailTextLabel?.text = dateFormatter.string(from: date)
-//            //                } else {
-//            //                    cell.detailTextLabel?.text = SettingsTableViewCell.NoValueString
-//            //                }
-//            //            case .trend:
-//            //                cell.textLabel?.text = LocalizedString("Trend", comment: "Title describing glucose trend")
-//            //
-//            //                cell.detailTextLabel?.text = glucose?.trendType?.localizedDescription ?? SettingsTableViewCell.NoValueString
-//            //            }
-//            //
-//        //            return cell
-//        case .delete:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: TextButtonTableViewCell.className, for: indexPath) as! TextButtonTableViewCell
-//            
-//            cell.textLabel?.text = LocalizedString("Delete CGM", comment: "Title text for the button to remove a CGM from Loop")
-//            cell.textLabel?.textAlignment = .center
-//            cell.tintColor = .delete
-//            cell.isEnabled = true
-//            return cell
-//        }
-//    }
-//    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch Section(rawValue: section)! {
+            //        case .authentication:
+            //            return 1
+            //        case .latestReading:
+        //            return LatestReadingRow.count
+        case .delete:
+            return 1
+        }
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch Section(rawValue: indexPath.section)! {
+            //        case .authentication:
+            //            let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.className, for: indexPath) as! SettingsTableViewCell
+            //
+            //            let service = cgmManager.shareService
+            //
+            //            cell.textLabel?.text = LocalizedString("Credentials", comment: "Title of cell to set credentials")
+            //            cell.detailTextLabel?.text = service.username ?? SettingsTableViewCell.TapToSetString
+            //            cell.accessoryType = .disclosureIndicator
+            //
+            //            return cell
+            //        case .latestReading:
+            //            let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.className, for: indexPath) as! SettingsTableViewCell
+            //            let glucose = cgmManager.latestBackfill
+            //
+            //            switch LatestReadingRow(rawValue: indexPath.row)! {
+            //            case .glucose:
+            //                cell.textLabel?.text = LocalizedString("Glucose", comment: "Title describing glucose value")
+            //
+            //                if let quantity = glucose?.quantity, let formatted = glucoseFormatter.string(from: quantity, for: glucoseUnit) {
+            //                    cell.detailTextLabel?.text = formatted
+            //                } else {
+            //                    cell.detailTextLabel?.text = SettingsTableViewCell.NoValueString
+            //                }
+            //            case .date:
+            //                cell.textLabel?.text = LocalizedString("Date", comment: "Title describing glucose date")
+            //
+            //                if let date = glucose?.timestamp {
+            //                    cell.detailTextLabel?.text = dateFormatter.string(from: date)
+            //                } else {
+            //                    cell.detailTextLabel?.text = SettingsTableViewCell.NoValueString
+            //                }
+            //            case .trend:
+            //                cell.textLabel?.text = LocalizedString("Trend", comment: "Title describing glucose trend")
+            //
+            //                cell.detailTextLabel?.text = glucose?.trendType?.localizedDescription ?? SettingsTableViewCell.NoValueString
+            //            }
+            //
+        //            return cell
+        case .delete:
+            let cell = tableView.dequeueReusableCell(withIdentifier: TextButtonTableViewCell.className, for: indexPath) as! TextButtonTableViewCell
+            
+            cell.textLabel?.text = LocalizedString("Delete Pump", comment: "Title text for the button to remove a Pump from Loop")
+            cell.textLabel?.textAlignment = .center
+            cell.tintColor = .delete
+            cell.isEnabled = true
+            return cell
+        }
+    }
+//
 //    public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 //        switch Section(rawValue: section)! {
 //            //        case .authentication:
@@ -192,24 +192,20 @@ class VirtualPumpSettingsViewController: UITableViewController {
 //    }
 }
 
-
 private extension UIAlertController {
-    convenience init(cgmDeletionHandler handler: @escaping () -> Void) {
+    convenience init(pumpDeletionHandler handler: @escaping () -> Void) {
         self.init(
             title: nil,
-            message: LocalizedString("Are you sure you want to delete this CGM?", comment: "Confirmation message for deleting a CGM"),
+            message: "Are you sure you want to delete this pump?",
             preferredStyle: .actionSheet
         )
         
         addAction(UIAlertAction(
-            title: LocalizedString("Delete CGM", comment: "Button title to delete CGM"),
+            title: "Delete Pump",
             style: .destructive,
-            handler: { (_) in
-                handler()
-        }
+            handler: { _ in handler() }
         ))
         
-        let cancel = LocalizedString("Cancel", comment: "The title of the cancel action in an action sheet")
-        addAction(UIAlertAction(title: cancel, style: .cancel, handler: nil))
+        addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
     }
 }
